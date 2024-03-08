d244e4871c66345e0a1edc074f661bb1bd86c0fc
henzyd <henzydee@gmail.com>
2024-03-08 14:36:49 +0100

    chore: created nav bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Footer from "~/components/footer";
 import Contact from "./(sections)/contact";
 import Publication from "./(sections)/publication";
+import Intro from "./(sections)/intro";
 
 export default function Home() {
   return (
     <main className="flex flex-col w-full flex-grow justify-between gap-4">
       <div className="flex-grow">
+        <Intro />
         <Publication />
         <Contact />
       </div>
---
+++ src/components/nav-bar.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import { HiOutlineUserCircle } from "react-icons/hi2";
+import Logo from "./logo";
+import Button from "./button";
+
+export default function NavBar() {
+  return (
+    <nav className="bg-transparent flex items-center w-full justify-between p-4 px-8 max-w-maxAppWidth mx-auto">
+      <Logo />
+
+      <ul className="flex items-center gap-6 [&_a]:text-info-800">
+        <li>
+          <Link href={"#about"} className="hover:underline">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link href={"#publication"} className="hover:underline">
+            Publication
+          </Link>
+        </li>
+        <li>
+          <Link href={"#contact"} className="hover:underline">
+            Contact
+          </Link>
+        </li>
+      </ul>
+
+      <Button className="text-white gap-2">
+        <HiOutlineUserCircle className="text-lg" />
+        <span className="font-normal">Sign Up</span>
+      </Button>
+    </nav>
+  );
+}
