7e23db0fb6c94586281692be2e27bb387e3bcec4
henzyd <henzydee@gmail.com>
2024-03-11 08:11:31 +0100

    chore: made intro section responsive

--- src/app/(sections)/intro.tsx
+++ src/app/(sections)/intro.tsx
@@ -9,27 +9,28 @@ export default function Intro() {
     <div className="bg-[#E7F3F7]">
       <NavBar />
       <div className="max-w-maxAppWidth mx-auto h-fit">
-        <div className="flex flex-col items-center gap-9 w-[75%] mx-auto pt-14">
-          <div className="flex flex-col items-center gap-3">
-            <h1 className="capitalize w-[70%] text-[3.2rem] text-info-950 font-bold text-center leading-[3.5rem]">
-              Welcome to our premium open data platform
+        <div className="flex flex-col items-center gap-9 mx-auto pt-14">
+          <div className="flex flex-col items-center gap-3 tabletAndBelow:gap-2 px-4">
+            <h1 className="capitalize text-[3.7vw] laptopAndAbove:text-[3rem] tablet:text-[5vw] largeMobile:!text-[6vw] text-info-950 font-bold text-center leading-[4.5vw] laptopAndAbove:leading-[3.5rem] tablet:leading-[6.3vw] largeMobile:!leading-[7.5vw]">
+              Welcome to our premium <br />
+              open data platform
             </h1>
-            <p className="text-center text-info-800 w-[55%] text-xl leading-6">
-              Harnessing the power of open data to shape a brighter future for
-              the Global South.
+            <p className="text-center text-info-800 text-base leading-6 tablet:text-xs largeMobile:!text-[3vw]">
+              Harnessing the power of open data to shape a brighter <br />{" "}
+              future for the Global South.
             </p>
           </div>
-          <Button className="w-fit gap-2 !px-16 !rounded-2xl">
-            <HiOutlineUserCircle className="text-lg" />
-            <span className="font-normal">Sign Up Now</span>
+          <Button className="w-fit gap-2 !px-16 tablet:!px-8 !rounded-2xl tablet:!rounded-xl largeMobile:!py-3">
+            <HiOutlineUserCircle className="text-lg largeMobile:text-base" />
+            <span className="font-normal tablet:text-xs">Sign Up Now</span>
           </Button>
-          <div className="overflow-hidden w-full aspect-[1/0.69]">
-            <div className="border-4 border-white rounded-[1.1rem] p-5 w-full bg-transparent">
+          <div className="overflow-hidden w-[80%] tablet:w-full px-4 aspect-[1/0.67] tablet:aspect-[1/0.6]">
+            <div className="border-4 largeMobile:border-2 border-white rounded-[1.1rem] p-5 largeMobile:p-2 w-full bg-transparent">
               <Image
                 src={DashboardImage}
                 priority
                 alt="Open South dashboard design"
-                className="w-full shadow-md rounded-[1.1rem] min-h-80"
+                className="w-full shadow-md rounded-[1.1rem]"
               />
             </div>
           </div>
