87323ab4210b709d9f9fed5fcb36f8346c5ed493
henzyd <henzydee@gmail.com>
2024-03-13 16:21:46 +0100

    chore: made navbar responsive

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -3,12 +3,35 @@
 import { useState } from "react";
 import Link from "next/link";
 import { Drawer, IconButton } from "@mui/material";
-import { IoMenu } from "react-icons/io5";
+import { IoClose, IoMenu } from "react-icons/io5";
 import Logo from "./logo";
 import Button from "./button";
 
+const links = [
+  {
+    to: "#features",
+    text: "Features",
+  },
+  {
+    to: "#about",
+    text: "About",
+  },
+  {
+    to: "#publication",
+    text: "Publication",
+  },
+  {
+    to: "#contact",
+    text: "Contact",
+  },
+];
+
 export default function NavBar() {
   const [showDrawer, setShowDrawer] = useState(false);
+
+  function closeDrawer() {
+    setShowDrawer(false);
+  }
 
   return (
     <>
@@ -18,26 +41,13 @@ export default function NavBar() {
         </Link>
 
         <ul className="flex items-center gap-6 [&_a]:text-info-800 tablet:hidden">
-          <li>
-            <Link href={"#features"} className="hover:underline">
-              Features
-            </Link>
-          </li>
-          <li>
-            <Link href={"#about"} className="hover:underline">
-              About
-            </Link>
-          </li>
-          <li>
-            <Link href={"#publication"} className="hover:underline">
-              Publication
-            </Link>
-          </li>
-          <li>
-            <Link href={"#contact"} className="hover:underline">
-              Contact
-            </Link>
-          </li>
+          {links.map((item, index) => (
+            <li key={index + 1}>
+              <Link href={item.to} className="hover:underline">
+                {item.text}
+              </Link>
+            </li>
+          ))}
         </ul>
         <Button className="!py-2 tablet:!hidden" href={"#contact"}>
           <span className="font-normal">Contact us</span>
@@ -50,31 +60,30 @@ export default function NavBar() {
         </IconButton>
       </nav>
 
-      <Drawer open={showDrawer} onClose={() => setShowDrawer(false)}>
-        <aside>
-          <ul className="flex items-center gap-6 [&_a]:text-info-800">
-            <li>
-              <Link href={"#features"} className="hover:underline">
-                Features
-              </Link>
-            </li>
-            <li>
-              <Link href={"#about"} className="hover:underline">
-                About
-              </Link>
-            </li>
-            <li>
-              <Link href={"#publication"} className="hover:underline">
-                Publication
-              </Link>
-            </li>
-            <li>
-              <Link href={"#contact"} className="hover:underline">
-                Contact
-              </Link>
-            </li>
+      <Drawer open={showDrawer} onClose={closeDrawer}>
+        <aside className="[@media(min-width:768px)]:hidden w-[350px] largeMobile:w-[290px] mediumMobile:!w-[250px] relative pt-24 pb-16 px-4 flex flex-col items-center gap-8 justify-between h-full">
+          <IconButton
+            className="w-fit !absolute !top-[16px] !right-[16px]"
+            onClick={() => {
+              setShowDrawer(false);
+            }}
+          >
+            <IoClose className="text-zinc-800" />
+          </IconButton>
+          <ul className="flex flex-col gap-8 [&_a]:text-info-800 items-center">
+            {links.map((item, index) => (
+              <li key={index + 1}>
+                <Link href={item.to} onClick={closeDrawer}>
+                  {item.text}
+                </Link>
+              </li>
+            ))}
           </ul>
-          <Button className="!py-2" href={"#contact"}>
+          <Button
+            className="!py-2 w-[200px]"
+            href={"#contact"}
+            onClick={closeDrawer}
+          >
             <span className="font-normal">Contact us</span>
           </Button>
         </aside>
