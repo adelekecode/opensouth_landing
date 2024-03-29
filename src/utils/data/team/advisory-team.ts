4782f05fd12eb214882ec471a2f05c7fdb1c91e7
henzyd <henzydee@gmail.com>
2024-03-29 22:13:06 +0100

    chore: added team advisory

--- src/utils/data/team/advisory-team.ts
+++ src/utils/data/team/advisory-team.ts
@@ -2,11 +2,14 @@
 
 import SoledadGuilera from "~/assets/images/about/advisory-team/soledad-guilera.jpg";
 import RedietAbebe from "~/assets/images/about/advisory-team/rediet-abebe.jpg";
+import SekouRemy from "~/assets/images/about/advisory-team/sekou-remy.jpg";
+import BunmiOtegbade from "~/assets/images/about/advisory-team/bunmi-otegbade.png";
 
 const advisoryTeam = [
   {
     fullName: "Rediet Abebe",
     picture: RedietAbebe,
+    role: "Lecturer, Goldman School of Public Policy",
     socials: [
       {
         name: "website",
@@ -17,10 +20,33 @@ const advisoryTeam = [
   {
     fullName: "Soledad Guilera",
     picture: SoledadGuilera,
+    role: "Junior Fellow at the Harvard Society of Fellows and Andrew Carnegie Fellow",
     socials: [
       {
         name: "linkedin",
         link: "https://www.linkedin.com/in/soledadguilera",
+      },
+    ],
+  },
+  {
+    fullName: "Sekou L. Remy",
+    picture: SekouRemy,
+    role: "Senior Research Scientist at Google",
+    socials: [
+      {
+        name: "linkedin",
+        link: "https://www.linkedin.com/in/sekoulremy/?originalSubdomain=ke",
+      },
+    ],
+  },
+  {
+    fullName: "Bunmi Otegbade",
+    picture: BunmiOtegbade,
+    role: "Senior Director, Global Peer-to-Peer at Ashoka",
+    socials: [
+      {
+        name: "linkedin",
+        link: "https://www.linkedin.com/in/bunmiotegbade",
       },
     ],
   },
